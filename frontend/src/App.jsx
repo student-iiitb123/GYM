700ff769de516a22384a46e95119799668a24213
Ashish singh Goutam <ashishgautam7696@gmail.com>
2025-08-01 22:41:27 +0530

    update in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Front from "./firstpage.jsx"
-function App() {
+import Gymmemer from "./gymmember.jsx" // Import your gymmemer component
+import { Routes, Route } from "react-router-dom"
 
+function App() {
   return (
-    <>
-     {/* <h1>gym interface</h1> */}
-     <Front/>
-    </>
+    <Routes>
+      <Route path="/" element={<Front />} />
+      <Route path="/gymmember" element={<Gymmemer />} />
+    </Routes>
   )
 }
 
