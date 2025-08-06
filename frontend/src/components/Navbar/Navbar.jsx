89d707f0d634b751df473f1c333e6ec16acef5e5
student-iiitb123 <sanyam272727@gmail.com>
2025-08-06 14:46:35 +0530

    some changes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import './Navbar.css';
-// import {Link} from "react-router-dom"
+import {Link} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  const navigate=useNavigate();
   return (
     <nav>
       <div>
-        <img src='logo.png' alt='logo' />
+        <img onClick={() => navigate('/home')} src='logo.png' alt='logo' />
       </div>
       <ul>
         {/* <Link to="/">Home</Link> */}
-        <li><a href="/home">Home</a></li>
-        {/* <li><Link to="/">Home</Link></li> */}
-        <li><a href="/classes">About Us</a></li>
-        <li><a href="/trainer">Trainers</a></li>
-        <li><a href="/membership">Membership</a></li>
-        <li><a href="/contact">Contact</a></li>
+        {/* <li><a href="/classes">About Us</a></li> */}
+        {/* <li><a href="/trainer">Trainers</a></li> */}
+        {/* <li><a href="/membership">Membership</a></li> */}
+        {/* <li><a href="/contact">Contact</a></li> */}
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/classes">About Us</Link></li>
+        <li><Link to="/trainer">Trainer</Link></li>
+        <li><Link to="/membership">Membership</Link></li>
+        <li><Link to="/Contact">Contact</Link></li>
+
       </ul>
     </nav>
   );
